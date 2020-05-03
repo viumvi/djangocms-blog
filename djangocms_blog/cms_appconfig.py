3f1de9561a8c4d42b39c6ea462c2dd886d83b6d0
Vladislav Yena <vladislav@what.digital>
2020-05-04 00:49:11 +0300

    Mode translations fields to the related models classes code

--- djangocms_blog/cms_appconfig.py
+++ djangocms_blog/cms_appconfig.py
@@ -5,6 +5,7 @@ from aldryn_apphooks_config.models import AppHookConfig
 from aldryn_apphooks_config.utils import setup_config
 from app_data import AppDataForm
 from django import forms
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from filer.models import ThumbnailOption
 from parler.models import TranslatableModel, TranslatedFields
@@ -27,6 +28,14 @@ class BlogConfigBase(AppHookConfig):
 
 class BlogConfig(TranslatableModel, BlogConfigBase):
     pass
+
+
+blog_config_translations = TranslatedFields(
+    app_title=models.CharField(_('application title'), max_length=234),
+    object_name=models.CharField(
+        _('object name'), max_length=234, default=get_setting('DEFAULT_OBJECT_NAME')
+    ),
+)
 
 
 class BlogConfigForm(AppDataForm):
--- djangocms_blog/models.py
+++ djangocms_blog/models.py
@@ -181,6 +181,16 @@ class BlogCategory(BlogCategoryAbstract):
         abstract = False
 
 
+blog_category_translations = TranslatedFields(
+    name=models.CharField(_('name'), max_length=752),
+    slug=models.SlugField(_('slug'), max_length=752, blank=True, db_index=True),
+    meta_description=models.TextField(
+        verbose_name=_('category meta description'), blank=True, default=''
+    ),
+    meta={'unique_together': (('language_code', 'slug'),)}
+)
+
+
 @python_2_unicode_compatible
 class PostAbstract(KnockerModel, BlogMetaMixin):
     """
@@ -438,6 +448,28 @@ class Post(PostAbstract):
 
     class Meta:
         abstract = False
+
+
+post_translations = TranslatedFields(
+    title=models.CharField(_('title'), max_length=752),
+    slug=models.SlugField(_('slug'), max_length=752, blank=True,
+                          db_index=True, allow_unicode=True),
+    subtitle=models.CharField(verbose_name=_('subtitle'), max_length=767,
+                              blank=True, default=''),
+    abstract=HTMLField(_('abstract'), blank=True, default='',
+                       configuration='BLOG_ABSTRACT_CKEDITOR'),
+    meta_description=models.TextField(verbose_name=_('post meta description'),
+                                      blank=True, default=''),
+    meta_keywords=models.TextField(verbose_name=_('post meta keywords'),
+                                   blank=True, default=''),
+    meta_title=models.CharField(verbose_name=_('post meta title'),
+                                help_text=_('used in title tag and social sharing'),
+                                max_length=2000,
+                                blank=True, default=''),
+    post_text=HTMLField(_('text'), default='', blank=True,
+                        configuration='BLOG_POST_TEXT_CKEDITOR'),
+    meta={'unique_together': (('language_code', 'slug'),)}
+)
 
 
 class BasePostPlugin(CMSPlugin):
