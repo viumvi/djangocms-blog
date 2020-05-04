9a0702e8f01a0380e088a44762865eab23fc1689
Vladislav Yena <vladislav@what.digital>
2020-05-04 23:22:59 +0300

    Experimenting with app_labels

--- djangocms_blog/models.py
+++ djangocms_blog/models.py
@@ -501,10 +501,10 @@ class Post(PostAbstract, TranslatableModel):
 
 
 class BasePostPlugin(CMSPlugin):
-    app_label2 = 'djangocms_blog'
+    app_label = 'djangocms_blog'
 
     app_config = AppHookConfigField(
-        get_model(app_label2, 'BlogConfig'), null=True, verbose_name=_('app. config'), blank=True,
+        get_model(app_label, 'BlogConfig'), null=True, verbose_name=_('app. config'), blank=True,
         related_name='%(app_label)s_app_config'
     )
     current_site = models.BooleanField(
@@ -554,10 +554,10 @@ class LatestPostsPluginAbstract(BasePostPlugin):
                                                    'articles to be displayed.'))
     tags = TaggableManager(_('filter by tag'), blank=True,
                            help_text=_('Show only the blog articles tagged with chosen tags.'),
-                           related_name='%(app_label)_latest_post')
+                           related_name='%(app_label)s_latest_post')
     categories = models.ManyToManyField(get_model(app_label, 'BlogCategory'), blank=True,
                                         verbose_name=_('filter by category'),
-                                        related_name='%(app_label)_categories',
+                                        related_name='%(app_label)s_categories',
                                         help_text=_('Show only the blog articles tagged '
                                                     'with chosen categories.'))
 
@@ -583,6 +583,7 @@ class LatestPostsPluginAbstract(BasePostPlugin):
 
 
 class LatestPostsPlugin(LatestPostsPluginAbstract):
+    app_label = 'djangocms_blog'
 
     class Meta(LatestPostsPluginAbstract.Meta):
         abstract = False
